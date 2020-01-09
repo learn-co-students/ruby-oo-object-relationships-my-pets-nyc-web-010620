04660c77650c968b6f5ada5a7b7cc00a43ce90ba
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-09 15:21:46 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood, :order
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @order = nil
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood, :order
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner=nil)
+    @name = name
+    @owner = owner
+    @order = nil
+    @mood = "nervous"
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
