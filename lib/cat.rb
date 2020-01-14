2c0f0fea2374a63753e6bf34af04e123004d909d
myk245 <myk245@nyu.edu>
2020-01-13 19:25:44 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood 
+
+  @@all = []
+
+  def initialize(name, owner = nil)
+    @name = name
+    @owner = owner 
+    @mood = "nervous"
+
+    @@all << self 
+  end 
+
+  def self.all
+    @@all
+  end 
+
 end
+
+# Cat Instance methods initializes with a name and an Owner
+# Cat Instance methods can change its owner
+# Cat Instance methods can't change its name
+# Cat Instance methods initializes with a nervous mood
+# Cat Instance methods can change its mood
+# Cat Class methods knows all the cats
